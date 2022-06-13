54bd32fac897c343dc45dbf8aa02399a08252050
Kirill Neruchev <kirill@neruchev.com>
2022-06-13 11:33:02 +0200

    feat(data): add scripts

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,1 +1,75 @@
-console.log('test');
+import { promises } from 'fs';
+import { join, sep } from 'path';
+
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const { readdir, writeFile } = promises;
+
+type FileName = {
+  category: string;
+  id: string;
+  path: string;
+  type: string;
+};
+
+const ROOT = 'docs';
+const LOCALES = ['ru', 'hbs'] as const;
+
+const getFilenames = async (directory: string): Promise<FileName[]> => {
+  const files: FileName[] = [];
+  const listing = await readdir(directory, { withFileTypes: true });
+
+  for (const item of listing) {
+    const path = join(directory, item.name);
+
+    if (!item.isFile()) {
+      files.push(...(await getFilenames(path)));
+    } else if (path.endsWith('index.json')) {
+      const [_, id, category, type] = path.split(sep).reverse();
+
+      files.push({ category, id, path, type });
+    }
+  }
+
+  return files;
+};
+
+(async () => {
+  const filenames = await getFilenames(ROOT);
+
+  const files = filenames.map((item) => ({
+    ...item,
+    file: require(join('..', item.path)),
+  }));
+
+  const data = LOCALES.map((locale) => ({
+    locale,
+    content: {
+      type: 'FeatureCollection',
+      features: files.map(({ file, category, id, type }) => ({
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [file.coordinates.longitude, file.coordinates.latitude],
+        },
+        properties: {
+          name: file.name[locale],
+          description: file.description[locale],
+          category,
+          notVerified: file.verified ? undefined : true,
+          path: `/${type}/${category}/${id}/`,
+        },
+      })),
+    },
+  }));
+
+  await Promise.all(
+    data.map(({ locale, content }) => {
+      const path = join(ROOT, `${locale}.json`);
+      const text = JSON.stringify(content);
+
+      return writeFile(path, text);
+    })
+  );
+})();
